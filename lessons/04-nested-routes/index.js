aec0ddf97705e9dae1068dbc254f994a0fb74909
wangdahoo <157195705@qq.com>
2016-07-04 00:51:11 +0800

    fix lesson4 code

--- lessons/04-nested-routes/index.js
+++ lessons/04-nested-routes/index.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
+import { Router, Route, browserHistory } from 'react-router'
+import App from './components/App'
 
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
-  </Router>
-), document.getElementById('app'))
+import About from './routes/About'
+import Repos from './routes/Repos'
+
+const routes = {
+  path: '/',
+  component: App,
+  //childRoutes: [
+  //  About,
+  //  Repos
+  //]
+
+  getChildRoutes(partialNextState, cb) {
+    require.ensure([], (require) => {
+      const About = require('./routes/About').default;
+      const Repos = require('./routes/Repos').default;
+
+      cb(null, [
+        // Provide store for async reducers and middleware
+        About,
+        Repos
+      ])
+    })
+  }
+}
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+)
+
+//import About from './routes/About/components/About'
+//import Repos from './routes/Repos/components/Repos'
+//render((
+//  <Router history={browserHistory}>
+//    <Route path="/" component={App}>
+//      <Route path="/repos" component={Repos} />
+//      <Route path="/about" component={About} />
+//    </Route>
+//  </Router>
+//), document.getElementById('app'))
---
+++ lessons/04-nested-routes/routes/About/index.js
@@ -0,0 +1,8 @@
+export default  {
+  path: 'about',
+  getComponent(nextState, cb) {
+    require.ensure([], (require) => {
+      cb(null, require('./components/About').default)
+    })
+  }
+}
